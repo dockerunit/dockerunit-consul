b570d6a175cfcdbc489bf5c27a2734fdb2f52309
fsorice <francesco.sorice@yoti.com>
2019-07-16 17:36:37 +0100

    Fix lombok-jackson serialization for ServiceRecord
    - In the version of lombok 1.16.18 there was an annotation that was used on the created ctors that is @ConstructorProperties.
      This annotation was used by jackson for serializating correctly the object, but in the new version of lombok, that is 1.18.8 that was not generated anymore.
      For this reason the serialization of the object ServiceRecord, without an empty ctor, was failing.
    
      Thanks to Iker that spotted that during the tests!
    
    Signed-off-by: fsorice <fsorice89@gmail.com>

--- src/main/java/com/github/dockerunit/discovery/consul/ServiceRecord.java
+++ src/main/java/com/github/dockerunit/discovery/consul/ServiceRecord.java
@@ -5,77 +5,81 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Wither;
 
 @Wither
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ServiceRecord {
 
-	@JsonProperty("ServiceName")
-	private final String name;
-	
-	@JsonProperty("Address")
-	private final String address;
-	
-	@JsonProperty("ServicePort")
-	private final int port;
+    @JsonProperty("ServiceName")
+    private String name;
 
-	@JsonProperty("ServiceAddress")
-	private String serviceAddress;
-	
-	@JsonProperty("Service")
-	private final Service service;
-	
-	@JsonProperty("Checks")
-	private final List<Check> checks;
-	
-	
-	public String getName() {
-		return service != null ? service.getName() : name; 
-	}
-	
-	public String getAddress() {
-		return service != null ? service.getAddress() : address;
-	}
-	
-	public int getPort() {
-		return service != null ? service.getPort() : port;
-	}
+    @JsonProperty("Address")
+    private String address;
 
-	@Wither
-	@Getter
-	@AllArgsConstructor
-	@JsonIgnoreProperties(ignoreUnknown = true)
-	public static class Service {
-		
-		@JsonProperty("Service")
-		private final String name;
-		
-		@JsonProperty("Address")
-		private final String address;
-		
-		@JsonProperty("Port")
-		private final int port;
-		
-	}
-	
-	@Wither
-	@Getter
-	@AllArgsConstructor
-	@JsonIgnoreProperties(ignoreUnknown = true)
-	public static class Check {
-		
-		public static final String PASSING = "passing";
+    @JsonProperty("ServicePort")
+    private int port;
 
-		@JsonProperty("Name")
-		private final String name;
-		
-		@JsonProperty("Status")
-		private final String status;
-		
-	}
+    @JsonProperty("ServiceAddress")
+    private String serviceAddress;
+
+    @JsonProperty("Service")
+    private Service service;
+
+    @JsonProperty("Checks")
+    private List<Check> checks;
+
+    public String getName() {
+        return service != null ? service.getName() : name;
+    }
+
+    public String getAddress() {
+        return service != null ? service.getAddress() : address;
+    }
+
+    public int getPort() {
+        return service != null ? service.getPort() : port;
+    }
+
+    @Wither
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Service {
+
+        @JsonProperty("Service")
+        private String name;
+
+        @JsonProperty("Address")
+        private String address;
+
+        @JsonProperty("Port")
+        private int port;
+
+    }
+
+    @Wither
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Check {
+
+        public static final String PASSING = "passing";
+
+        @JsonProperty("Name")
+        private String name;
+
+        @JsonProperty("Status")
+        private String status;
+
+    }
 }
